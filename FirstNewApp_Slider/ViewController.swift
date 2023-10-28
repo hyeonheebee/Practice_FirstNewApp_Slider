749d1c97285b2119e87535c511fb42821aaceff4
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-28 23:20:16 +0900

    기본 설정(UI구성, 컴포넌트 연결)_내부 함수 미작성

--- FirstNewApp_Slider/ViewController.swift
+++ FirstNewApp_Slider/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        
+
+    }
+    
+    
+    // 일반적으로 viewDidLoad에 들어가야할 내용을 따로 함수로 빼서 정리해줌
+    
+    func configureUI(){
+        mainLabel.text = "초를 선택하세요"
     }
-
-
+
+    // 슬라이더 값이 변할때마다 해당 함수가 호출됨
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+    }
+    
+    // 버튼이 tap될때마다 해당 함수가 호출됨
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
